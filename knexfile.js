3d627e5f273accdbbbed3f46067dbde2372bd2d1
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2020-05-01 16:42:15 -0300

    Adds database configuration

---
+++ knexfile.js
@@ -0,0 +1,45 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: 'localhost',
+      database: 'savefy',
+      user: 'postgres',
+      password: 'postgres',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
+    },
+    seeds: {
+      directory: './src/database/seeds',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
+    },
+    seeds: {
+      directory: './src/database/seeds',
+    },
+  },
+};
