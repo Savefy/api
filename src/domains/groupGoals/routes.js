4c9aa10d02ef57e1e552961393cb776f2312fdd9
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2020-05-03 16:22:57 -0300

    Adds route to input credit in group goal

--- src/domains/groupGoals/routes.js
+++ src/domains/groupGoals/routes.js
@@ -3,5 +3,6 @@ const controller = require('./controller');
 
 router.post('/users/:userId/groupGoals', controller.create);
 router.get('/users/:userId/groupGoals', controller.getByUser);
+router.post('/groups/:groupId/goals/:goalId/credit', controller.credit);
 
 module.exports = router;
