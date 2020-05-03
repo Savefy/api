f08e3d03e4fba8a5cbde0d1c59640a0363b95aab
dennisurtubia <dennisurtubia@alunos.utfpr.edu.br>
2020-05-03 01:18:20 -0300

    Updates configurations

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: 'localhost',
-      database: 'savefy',
-      user: 'postgres',
-      password: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
